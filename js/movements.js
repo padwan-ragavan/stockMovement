1362888d831d09efd744aa86944ea8c2c6ae0097
Ragavan Govindarajan <ragavanspd@gmail.com>
2013-10-18 04:51:49 +0530

    bubble chart for product movements

--- js/movements.js
+++ js/movements.js
@@ -1,30 +1,27 @@
 var countryCoordinates = {
-    "GB" : {latitude: 53.5500, longitude: 2.4333},
+    "GB" : {latitude: 52.2050, longitude: 0.1190},
+    "UK" : {latitude: 52.2050, longitude: 0.1190},
     "CN" : {latitude: 35.0000, longitude: 103.0000},
     "JP" : {latitude: 35.0000, longitude: 136.0000},
     "US" : {latitude: 40.0000, longitude: -100.0000},
 };
 
-var stockMovements = [
-        {
-            origin: 'GB',
-            destination : 'CN'
-        },
-        {
-            origin: 'CN',
-            destination : 'US'
-        }
-    ];
-
 function getMovements(){
-    return _.map(stockMovements, function(m){
-            return {
-                    origin : toCoordinates(m.origin),
-                    destination:  toCoordinates(m.destination)
-                };
+    return $.getJSON('http://localhost:3211/StockMovement')
+            .then(function(data){
+                return _.map(data, function(m){
+                                return $.extend({},m,toCoordinates(m.origin), {radius: m.count, originCode: m.origin});
+                            });
     });   
 }
 
 function toCoordinates(country){
     return countryCoordinates[country];
+}
+
+function getMovementPerCountry(){
+    return getMovements()
+            .then(function(data){
+                return data;
+            });
 }
